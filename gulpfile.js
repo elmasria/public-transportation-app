ee69685bc7ff3fec2aa17765f2cb74549c30a767
Ahmad EL Masri <ahmadjelmasri@gmail.com>
2016-09-23 00:09:14 +0300

    chore: Add required libraries, initialize homepage

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,17 @@
 var gulp = require('gulp')
-browserSync = require('browser-sync').create(),
-clean = require('gulp-clean');
+	uglify = require('gulp-uglify'),
+	strip = require('gulp-strip-comments'),
+	htmlmin = require('gulp-htmlmin'),
+	removeHtmlComments = require('gulp-remove-html-comments'),
+	stripCssComments = require('gulp-strip-css-comments'),
+	cleanCSS = require('gulp-clean-css'),
+	cssmin = require('gulp-cssmin'),
+	autoprefixer = require('gulp-autoprefixer'),
+	concat = require('gulp-concat'),
+	rename = require('gulp-rename'),
+	clean = require('gulp-clean'),
+	browserSync = require('browser-sync').create(),
+	reload = browserSync.reload;
 
 
 var paths = {
@@ -12,6 +23,9 @@ paths.angular = paths.node_module + 'angular/angular.js';
 paths.angularRoute = paths.node_module + 'angular-route/angular-route.js';
 paths.angularCookies = paths.node_module + 'angular-cookies/angular-cookies.js';
 paths.angularAnimate = paths.node_module + 'angular-animate/angular-animate.js';
+
+paths.jquery = paths.node_module + 'jquery/dist/jquery.js';
+
 paths.bootstrapCSS = paths.node_module + 'bootstrap/dist/css/bootstrap.css';
 paths.bootstrapJS = paths.node_module + 'bootstrap/dist/js/bootstrap.js';
 paths.bootstrapFonts = paths.node_module + 'bootstrap/dist/fonts/*';
@@ -21,6 +35,9 @@ paths.jsDest = paths.webroot + 'js';
 paths.cssDest = paths.webroot + 'css';
 paths.fontDest = paths.cssDest + '/fonts';
 paths.imagDest = paths.webroot + 'images';
+
+
+gulp.task('default',['minify-html','server']);
 
 gulp.task('server', function () {
 	browserSync.init({
@@ -32,6 +49,8 @@ gulp.task('server', function () {
 			port: 2100
 		}
 	});
+
+	gulp.watch('./src/index.html', ['minify-html']);
 });
 
 gulp.task('clean', function () {
@@ -39,3 +58,10 @@ gulp.task('clean', function () {
 	.pipe(clean({ force: true }));
 });
 
+gulp.task('minify-html', function() {
+	return gulp.src('./src/index.html')
+	.pipe(removeHtmlComments())
+	.pipe(htmlmin({collapseWhitespace: true}))
+	.pipe(gulp.dest(paths.webroot))
+	.pipe(reload({stream: true}));
+});
